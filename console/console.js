27c88d232fe12a44f485463df3dd6f4dcf1c1bce
selva.duraipandian <selva@kissflow.com>
2022-10-05 23:03:44 +0530

    console learning

---
+++ console/console.js
@@ -0,0 +1,70 @@
+
+//console.log//////////////////////////
+// const name = 'fatfish'
+// const age = 24
+
+// console.log(name, age)
+
+
+//console.log with CSS effect//////////////////////////
+// for (let i = 0; i < 50; i++) {
+//     console.log(`i: ${i}`)
+
+//     if (i === 16) {
+//         console.log(`%ci--: ${i}`, 'background-color: yellow;font-weight:bold;color:red;')
+//     }
+// }
+
+//console.error//////////////////////////
+// const a = () => {
+//     console.error("Selva");
+// }
+// const b = () => {
+//     a()
+// }
+// const c = () => {
+//     b()
+// }
+
+// c()
+
+
+//console.time//////////////////////////
+// let count = 0
+
+// console.time('time1') //timer switched on
+
+// for (let i = 0; i < 1000000000; i++) {
+//     count++
+// }
+
+// console.timeEnd('time1') //timer switched off
+//timer on is required to find the execution time
+
+
+
+//console.table//////////////////////////
+const foods = [
+    {
+        name: '🍔',
+        price: 30.89,
+        group: 1,
+    },
+    {
+        name: '🍨',
+        price: 20.71,
+        group: 1,
+    },
+    {
+        name: '🍿',
+        price: 10.31,
+        group: 2,
+    },
+    {
+        name: '🍵',
+        price: 5.98,
+        group: 2,
+    },
+]
+
+console.table(foods)
